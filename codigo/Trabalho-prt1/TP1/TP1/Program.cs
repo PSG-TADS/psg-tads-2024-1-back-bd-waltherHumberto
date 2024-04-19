030f584201c07879e242b768616da81fa644ea19
waltherHumberto <70277063+waltherHumberto@users.noreply.github.com>
2024-04-19 01:36:53 -0300

    Coloquei o codigo padrao do swagger mas ainda sem funcionar

--- codigo/Trabalho-prt1/TP1/TP1/Program.cs
+++ codigo/Trabalho-prt1/TP1/TP1/Program.cs
@@ -17,12 +17,23 @@ namespace LocadoraVeiculos
             builder.Services.AddDbContext<ApplicationContext>();
 
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
+
             app.UseAuthorization();
+
             app.MapControllers();
 
             app.Run();
